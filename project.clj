1d052eaedb580543fb5fd72ee4837a7cff3057ee
Allen Rohner <arohner@gmail.com>
2014-01-16 11:27:10 -0800

    Bump deps. Include data.xml, because lein currently depends on 0.0.3, overwriting our preference

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject lein-deploy-deps "0.1.0"
+(defproject lein-deploy-deps "0.1.1-SNAPSHOT"
   :description "Deploy project dependencies to a remote repository"
   :url "https://github.com/neatonk/lein-deploy-deps"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :dependencies [[org.clojure/data.zip "0.1.1" :exclusions [org.clojure/clojure]]
+                 ;; currently ignored because lein depends on 0.0.3
+                 [org.clojure/data.xml "0.0.7"]])
