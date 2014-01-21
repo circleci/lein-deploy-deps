83f96b6f6ba814f32a776cea6273013463806459
Allen Rohner <arohner@gmail.com>
2014-01-20 19:11:52 -0800

    Bump the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-deploy-deps "0.1.2"
+(defproject circle/lein-deploy-deps "0.1.3"
   :description "Deploy project dependencies to a remote repository"
   :url "https://github.com/neatonk/lein-deploy-deps"
   :license {:name "Eclipse Public License"
